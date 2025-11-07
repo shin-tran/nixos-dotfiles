0bb725d37d917014f3eff28ddf0e256fa937e5d1
shin-tran <ngoc.shintrann@gmail.com>
2025-11-08 00:42:45 +0700

    feat: Add default locale and extra locale settings for IBus configuration

--- modules/nixos/services/ibus.nix
+++ modules/nixos/services/ibus.nix
@@ -11,6 +11,20 @@
     ];
   };
 
+  i18n.defaultLocale = "en_US";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US";
+    LC_IDENTIFICATION = "en_US";
+    LC_MEASUREMENT = "en_US";
+    LC_MONETARY = "en_US";
+    LC_NAME = "en_US";
+    LC_NUMERIC = "en_US";
+    LC_PAPER = "en_US";
+    LC_TELEPHONE = "en_US";
+    LC_TIME = "en_US";
+  };
+
   # Set environment variables for IBus
   environment.variables = {
     GTK_IM_MODULE = "ibus";
