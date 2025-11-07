5f5b6c4b66a8d5bc1af8cef75cd35d325898eadd
shin-tran <ngoc.shintrann@gmail.com>
2025-11-08 02:46:56 +0700

    feat: Update IBus configuration for improved locale settings and clarity

--- modules/nixos/services/ibus.nix
+++ modules/nixos/services/ibus.nix
@@ -1,28 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  # Input method for Vietnamese (and other languages)
   i18n.inputMethod = {
     enable = true;
     type = "ibus";
     ibus.engines = with pkgs.ibus-engines; [
-      bamboo  # Vietnamese input method
-      # unikey  # Alternative Vietnamese input (if needed)
+      bamboo
     ];
   };
 
-  i18n.defaultLocale = "en_US";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US";
-    LC_IDENTIFICATION = "en_US";
-    LC_MEASUREMENT = "en_US";
-    LC_MONETARY = "en_US";
-    LC_NAME = "en_US";
-    LC_NUMERIC = "en_US";
-    LC_PAPER = "en_US";
-    LC_TELEPHONE = "en_US";
-    LC_TIME = "en_US";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   # Set environment variables for IBus
