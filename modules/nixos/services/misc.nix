8b0db9f38d49332083e40f9bf86923b6f9b00a0e
shin-tran <ngoc.shintrann@gmail.com>
2025-11-08 01:25:20 +0700

    feat: Add libinput configuration for improved mouse and touchpad performance

--- modules/nixos/services/misc.nix
+++ modules/nixos/services/misc.nix
@@ -10,5 +10,12 @@
     power-profiles-daemon.enable = true;
     udev.packages = [ pkgs.android-udev-rules ];
     openssh.enable = true;
+
+    # Không gia tốc - thường tốt cho chơi game FPS
+    libinput = {
+      enable = true;
+      mouse.accelProfile = "flat";
+      touchpad.accelProfile = "flat";
+    };
   };
 }
