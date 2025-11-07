a4f44112264919a2edc5a0c5dff6e2026a0ddde1
shin-tran <ngoc.shintrann@gmail.com>
2025-11-07 12:53:32 +0700

    fix: Correct package name for Thunar in packages.nix

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -23,7 +23,7 @@
     wofi               # App launcher
     dunst              # Notification daemon
     wl-clipboard       # Clipboard manager
-    
+
     # Screenshot & recording
     grim               # Screenshot
     slurp              # Screen area selector
@@ -35,16 +35,16 @@
     pamixer            # Volume control (CLI)
     pavucontrol        # Volume control (GUI)
     networkmanagerapplet  # Network manager GUI
-    
+
     # File managers
-    thunar             # GUI file manager
+    xfce.thunar             # GUI file manager
     lf                 # Terminal file manager
-    
+
     # Search tools (needed for nvim telescope)
     ripgrep
     fd
     fzf
-    
+
     # System monitoring
     btop
     htop
