1a0805945c19cf0c5a8202fd03304d81e81a610e
shin-tran <ngoc.shintrann@gmail.com>
2025-11-08 22:40:20 +0700

    feat: Refactor i3 configuration for improved keybindings and startup commands

--- modules/home-manager/desktop/i3.nix
+++ modules/home-manager/desktop/i3.nix
@@ -15,47 +15,62 @@
   xsession.windowManager.i3 = {
     enable = true;
 
-    # package = pkgs.i3;
-
-    config = {
+    config = rec {
       modifier = "Mod4";
       terminal = "kitty";
       menu = "rofi -show drun -show-icons";
 
-      keybindings = {
-        # Mở terminal
-        "Mod4+Return" = "exec $terminal";
+      keybindings = let
+        mod = modifier;
+      in lib.mkOptionDefault {
+        # Terminal
+        "${mod}+Return" = "exec ${terminal}";
 
-        # Mở menu (bạn cũng đã định nghĩa $menu nhưng chưa gán)
-        "Mod4+d" = "exec $menu";
+        # App launcher
+        "${mod}+d" = "exec ${menu}";
 
-        # Thêm các phím tắt khác của bạn ở đây...
-        # Ví dụ: Đóng cửa sổ
-        "Mod4+q" = "kill";
+        # Kill window
+        "${mod}+Shift+q" = "kill";
 
-        # Chuyển sang workspace
-        # "Mod4+1" = "workspace number 1";
-        # "Mod4+2" = "workspace number 2";
-        # "Mod4+3" = "workspace number 3";
-        # "Mod4+4" = "workspace number 4";
-        # "Mod4+5" = "workspace number 5";
-        # "Mod4+6" = "workspace number 6";
-        # "Mod4+7" = "workspace number 7";
-        # "Mod4+8" = "workspace number 8";
-        # "Mod4+9" = "workspace number 9";
-        # "Mod4+0" = "workspace number 10";
-        #
-        # # Di chuyển cửa sổ sang workspace
-        # "Mod4+Shift+1" = "move container to workspace number 1";
-        # "Mod4+Shift+2" = "move container to workspace number 2";
-        # "Mod4+Shift+3" = "move container to workspace number 3";
-        # "Mod4+Shift+4" = "move container to workspace number 4";
-        # "Mod4+Shift+5" = "move container to workspace number 5";
-        # "Mod4+Shift+6" = "move container to workspace number 6";
-        # "Mod4+Shift+7" = "move container to workspace number 7";
-        # "Mod4+Shift+8" = "move container to workspace number 8";
-        # "Mod4+Shift+9" = "move container to workspace number 9";
-        # "Mod4+Shift+0" = "move container to workspace number 10";
+        # Reload/restart i3
+        "${mod}+Shift+c" = "reload";
+        "${mod}+Shift+r" = "restart";
+
+        # Focus
+        "${mod}+h" = "focus left";
+        "${mod}+j" = "focus down";
+        "${mod}+k" = "focus up";
+        "${mod}+l" = "focus right";
+
+        # Move
+        "${mod}+Shift+h" = "move left";
+        "${mod}+Shift+j" = "move down";
+        "${mod}+Shift+k" = "move up";
+        "${mod}+Shift+l" = "move right";
+
+        # Workspaces
+        "${mod}+1" = "workspace number 1";
+        "${mod}+2" = "workspace number 2";
+        "${mod}+3" = "workspace number 3";
+        "${mod}+4" = "workspace number 4";
+        "${mod}+5" = "workspace number 5";
+        "${mod}+6" = "workspace number 6";
+        "${mod}+7" = "workspace number 7";
+        "${mod}+8" = "workspace number 8";
+        "${mod}+9" = "workspace number 9";
+        "${mod}+0" = "workspace number 10";
+
+        # Move to workspace
+        "${mod}+Shift+1" = "move container to workspace number 1";
+        "${mod}+Shift+2" = "move container to workspace number 2";
+        "${mod}+Shift+3" = "move container to workspace number 3";
+        "${mod}+Shift+4" = "move container to workspace number 4";
+        "${mod}+Shift+5" = "move container to workspace number 5";
+        "${mod}+Shift+6" = "move container to workspace number 6";
+        "${mod}+Shift+7" = "move container to workspace number 7";
+        "${mod}+Shift+8" = "move container to workspace number 8";
+        "${mod}+Shift+9" = "move container to workspace number 9";
+        "${mod}+Shift+0" = "move container to workspace number 10";
       };
 
       bars = [
@@ -65,16 +80,26 @@
         }
       ];
 
-      # startup = [
-      #   { command = "${pkgs.feh}/bin/feh --bg-fill ~/nixos-dotfiles/walls/Anime-Girl-Rain.png"; always = true; notification = true; }
-      #   { command = "picom -b"; notification = false; }
-      #   { command = "fcitx5 -d"; notification = false; }
-      # ];
+      startup = [
+        {
+          command = "${pkgs.feh}/bin/feh --bg-fill ~/nixos-dotfiles/walls/Anime-Girl-Rain.png";
+          always = true;
+          notification = false;
+        }
+        {
+          command = "${pkgs.picom}/bin/picom -b";
+          notification = false;
+        }
+        {
+          command = "fcitx5 -d";
+          notification = false;
+        }
+      ];
 
-      # gap = {
-      #   inner = 5;
-      #   outer = 5;
-      # };
+      gaps = {
+        inner = 5;
+        outer = 5;
+      };
     };
   };
 }
